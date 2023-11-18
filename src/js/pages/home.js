7e79b0264baec8a68690ebf717d910641b05e9d7
NicolasCBV <nicolascleiton700e@gmail.com>
2023-11-18 17:42:48 -0300

    feat(home): adicionando estilização do modal na home

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,3 +1,26 @@
 import { LoginButtonHandler } from "../utils/loginButton.js";
 
 LoginButtonHandler.trySwitchToConfigButton();
+
+class Home {
+  static buildDescriptionModal = () => `
+    <span id="description-modal">
+      <div>
+        <h2>Previne doenças</h2>
+        <p>
+          A prática continua de exercícios ajuda qualquer indivíduo, principalmente os idosos, a se prevenir de doenças graves, como: câncer, diabete tipo 2 e doenças cardiovasculares.
+        </p>
+        <button class="colored-button">
+          Voltar
+        </button>
+      </div>
+    </span>
+  `
+
+  static watchDescriptionButtons() {
+    const button = document.querySelectorAll(".benefits-text .colored-button");
+    console.log(button)
+  }
+}
+
+Home.watchDescriptionButtons();
