98892f862a2a9b2e29ab60357c03e75834c2a7df
Antônio Fernando <fernandocosta@alu.ufc.br>
2023-11-25 10:01:31 -0300

    feat(alterando o código js)

---
+++ src/js/events/menushow.js
@@ -0,0 +1,9 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function ToggleMenu() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+
+btnMobile.addEventListener('click', ToggleMenu);
